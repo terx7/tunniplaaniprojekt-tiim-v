a4b9e50c9421ff8c89310d8330ad0b466ed65ef0
terx7 <55434235+terx7@users.noreply.github.com>
2020-11-05 12:48:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
-let teachersDict = {}
-let roomsDict = {}
 let groupDict = {}
 let weeksDict = {}
 const currentWeek = 9
 const groups = document.querySelector('#groups')
+const groupNames = document.querySelector('#groupNames')
+const timetable = document.querySelector('.timetable')
+const container = document.querySelector('.container')
+let groupId = 3148
 
 
-
-
-fetch('https://be.ta19heinsoo.itmajakas.ee/api/rooms').then(response => {
-    return response.json()
-}).then(data => {
-    data.forEach(rooms => {
-        roomsDict[rooms.roomId] = rooms.code
-    })
-})
-
-fetch('https://be.ta19heinsoo.itmajakas.ee/api/teachers').then(response => {
-    return response.json()
-}).then(data => {
-    data.forEach(teachers => {
-        teachersDict[teachers.teacherId] = {
-            'firstname': teachers.firstname,
-            'lastname': teachers.lastname
-        }
-    });
-})
 
 fetch('https://be.ta19heinsoo.itmajakas.ee/api/groups').then(response => {
     return response.json()
@@ -33,44 +15,77 @@ fetch('https://be.ta19heinsoo.itmajakas.ee/api/groups').then(response => {
     data.forEach(group => {
         groupDict[group.groupId] = group.groupCode
         let newDiv = document.createElement("option")
-        newDiv.innerHTML = "<option value=" + group.groupId + ">" + groupDict[group.groupId] + "</option>"
+        newDiv.value = group.groupId
+        newDiv.innerHTML = groupDict[group.groupId]
         groups.appendChild(newDiv)
 
     });
 })
 
-fetch('https://be.ta19heinsoo.itmajakas.ee/api/weeks').then(response => {
+fetch('https://be.ta19heinsoo.itmajakas.ee/api/weeks/' + currentWeek).then(response => {
     return response.json()
 }).then(data => {
-    data.forEach(weeks => {
-        weeksDict[weeks.weekNr] = {
-            'start': weeks.start,
-            'end': weeks.end
-        }
+    console.log(data)
+    console.log(data.start)
+    list = data.start.split('T')
+    list2 = list[0].split('-')
+    console.log(list2[2])
+    x = parseInt(list2[1])
+    list3 = []
+    for (let x = 0; x < 8; x++) {
+        const element = list3[x];
+        console.log(element)
+
+    }
+    let newBox = document.createElement('div')
+    newBox.classList.add("days")
+    newBox.innerHTML = `
+        <button value=${parseInt(list2[2])} class="monday">E</button>
+        <button value=${parseInt(list2[2]) + 1} class="tuesday">T</button>
+        <button value=${parseInt(list2[2]) + 2} class="wednesday">K</button>
+        <button value=${parseInt(list2[2]) + 3} class="thursday">N</button>
+        <button value=${parseInt(list2[2]) + 4} class="friday">R</button>
+        <button value=${parseInt(list2[2]) + 5} class="saturday">L</button>
+        <button value=${parseInt(list2[2]) + 6} class="sunday">P</button>`
+    container.appendChild(newBox)
+})
+
+
+fetch('https://be.ta19heinsoo.itmajakas.ee/api/lessons/groups=' + groupId + '&weeks=9').then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data)
+    console.log(data.timetableEvents)
+    data.timetableEvents.forEach(el => {
+        let newLesson = document.createElement('div')
+        newLesson.classList.add("tund")
+        newLesson.innerHTML = `<div class="leftside">
+        <span class="aeg">${el.timeStart}<br>14.15</span>
+        <br>
+    <span class="tunninr"></span>
+    </div>
+    <div class="rightside">
+        <span class="pealkiri">${el.nameEn}</span>
+        <span class="ruum">${el.rooms[0].roomCode}</span>
+        <span class="op">${el.teachers[0].name}</span>
+    </div>
+    <div class="side">
+    </div>`
+        timetable.appendChild(newLesson)
     })
 })
 
 
 
-function getLesson() {
-    console.log(groupName.value)
-    console.log(groupDict)
-    if (parseInt(groupName.value) in groupDict) {
-        console.log("yep")
-    }
-    console.log(Object.keys(groupDict))
-    for (let x = 0; x < Object.keys(groupDict).length; x++) {
-        const element = groupDict[x];
-        console.log(groupDict[x])
-
-    }
-}
-
 function datetime() {
 }
 
 
-console.log(teachersDict)
-console.log(roomsDict)
 console.log(groupDict)
 console.log(weeksDict)
+
+
+groups.addEventListener('change', event => {
+    selectedElement = event.target.value
+    getLessons(selectedElement)
+})
