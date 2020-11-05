8e801446ec7869b9da1989eefdaeb0066ce286bd
terx7 <55434235+terx7@users.noreply.github.com>
2020-11-05 15:45:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let groupDict = {}
+let groupDict = []
 let weeksDict = {}
-const currentWeek = 9
+const currentWeek = 8
 const groups = document.querySelector('#groups')
 const groupNames = document.querySelector('#groupNames')
 const timetable = document.querySelector('.timetable')
@@ -13,11 +13,8 @@ fetch('https://be.ta19heinsoo.itmajakas.ee/api/groups').then(response => {
     return response.json()
 }).then(data => {
     data.forEach(group => {
-        groupDict[group.groupId] = group.groupCode
-        let newDiv = document.createElement("option")
-        newDiv.value = group.groupId
-        newDiv.innerHTML = groupDict[group.groupId]
-        groups.appendChild(newDiv)
+        groupDict['groupCode'] = group.groupCode
+        groupDict['groupId'] = group.groupId
 
     });
 })
@@ -40,29 +37,31 @@ fetch('https://be.ta19heinsoo.itmajakas.ee/api/weeks/' + currentWeek).then(respo
     let newBox = document.createElement('div')
     newBox.classList.add("days")
     newBox.innerHTML = `
-        <button value=${parseInt(list2[2])} class="monday">E</button>
-        <button value=${parseInt(list2[2]) + 1} class="tuesday">T</button>
-        <button value=${parseInt(list2[2]) + 2} class="wednesday">K</button>
-        <button value=${parseInt(list2[2]) + 3} class="thursday">N</button>
-        <button value=${parseInt(list2[2]) + 4} class="friday">R</button>
-        <button value=${parseInt(list2[2]) + 5} class="saturday">L</button>
-        <button value=${parseInt(list2[2]) + 6} class="sunday">P</button>`
+        <button  value="2020-10-26T00:00:00Z"  class="monday">E</button>
+        <button value="2020-10-27T00:00:00Z" class="tuesday">T</button>
+        <button value="2020-10-28T00:00:00Z" onclick="location.href='index2.html'" class="wednesday">K</button>
+        <button value="2020-10-29T00:00:00Z" onclick="location.href='index.html'" class="thursday">N</button>
+        <button onclick=something() value="2020-10-30T00:00:00Z" onclick="location.href='index4.html'" class="friday">R</button>
+        <button value="2020-10-31T00:00:00Z" onclick="location.href='index3.html'" class="saturday">L</button>
+        <button value="2020-10-01T00:00:00Z" class="sunday">P</button>`
     container.appendChild(newBox)
 })
 
 
-fetch('https://be.ta19heinsoo.itmajakas.ee/api/lessons/groups=' + groupId + '&weeks=9').then(response => {
+fetch('https://be.ta19heinsoo.itmajakas.ee/api/lessons/groups=' + groupId + '&weeks=8').then(response => {
     return response.json()
 }).then(data => {
     console.log(data)
     console.log(data.timetableEvents)
     data.timetableEvents.forEach(el => {
-        let newLesson = document.createElement('div')
-        newLesson.classList.add("tund")
-        newLesson.innerHTML = `<div class="leftside">
-        <span class="aeg">${el.timeStart}<br>14.15</span>
+        console.log(el)
+        function dosomething() {
+            let newLesson = document.createElement('div')
+            newLesson.classList.add("tund")
+            newLesson.innerHTML = `<div class="leftside">
+        <span class="aeg">${el.timeStart}<br>${el.timeEnd}</span>
         <br>
-    <span class="tunninr"></span>
+    <span class="tunninr">${el.date}</span>
     </div>
     <div class="rightside">
         <span class="pealkiri">${el.nameEn}</span>
@@ -71,7 +70,12 @@ fetch('https://be.ta19heinsoo.itmajakas.ee/api/lessons/groups=' + groupId + '&we
     </div>
     <div class="side">
     </div>`
-        timetable.appendChild(newLesson)
+            timetable.appendChild(newLesson)
+        }
+        if (el.date == "2020-10-29T00:00:00Z") {
+            dosomething()
+        }
+
     })
 })
 
@@ -82,10 +86,21 @@ function datetime() {
 
 
 console.log(groupDict)
-console.log(weeksDict)
+console.log(location.pathname)
 
+function search() {
+    fetch('https://be.ta19heinsoo.itmajakas.ee/api/groups').then(response => {
+        return response.json()
+    }).then(data => {
+        const options = {
+            includeScore: true,
+            // Search in `author` and in `tags` array
+            keys: ['groupId', 'GroupCode']
+        }
 
-groups.addEventListener('change', event => {
-    selectedElement = event.target.value
-    getLessons(selectedElement)
-})
+        const fuse = new Fuse(groupDict, options)
+
+        const result = fuse.search('tak')
+        console.log(result)
+    })
+}
